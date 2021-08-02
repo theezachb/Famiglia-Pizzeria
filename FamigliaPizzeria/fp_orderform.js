41a22c2fc563de47ee4abc3439a1f497dbab9170
theezachb <44374200+theezachb@users.noreply.github.com>
2021-08-02 14:16:26 -0400

    Add files via upload
    customer and build order pages added

---
+++ FamigliaPizzeria/fp_orderform.js
@@ -0,0 +1,26 @@
+window.onload = init;
+
+function init() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("Data passes initial validation tests");
+      return false;
+   }
+   
+   document.getElementById("delivery").onclick = turnOnDelivery;
+   document.getElementById("pickup").onclick=turnOnPickup;
+}
+
+function turnOnDelivery() {
+   document.getElementById("addressBox").disabled=false;
+   document.getElementById("delBox").disabled=false;
+   document.getElementById("pickupBox").disabled=true;
+}
+
+function turnOnPickup() {
+   document.getElementById("addressBox").disabled=true;
+   document.getElementById("delBox").disabled=true;
+   document.getElementById("pickupBox").disabled=false;
+}
+   
+
+/* Build Order script */
